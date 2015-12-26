78d6755515329c4452d32c890d356dd155d9e4ab
Yijun Blue Yuan <Blue.Yuan@morningstar.com>
2015-12-26 05:15:34 +0000

    update code

--- ui.R
+++ ui.R
@@ -15,11 +15,7 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      textInput("title", "Enter something as chart title")
     ),
 
     # Show a plot of the generated distribution
